51452360af72d0f2d8d52b176070fedf9999fea7
drewd5555 <114956578+drewd5555@users.noreply.github.com>
2024-10-03 17:40:54 -0500

    Assignment 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Everyone, this is my starting code'));
+app.get('/', (req, res) => res.send('Hello Everyone, Im going to push out a change'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
