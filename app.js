d0ec0d8653b6c3fb463dc87fb82dfdc140eaad80
drewd5555 <114956578+drewd5555@users.noreply.github.com>
2024-10-03 17:28:02 -0500

    Starting Code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Everyone, I am finally here! I really hope this ends up working fr fr. Update: It just did, just gonna do a manual review now'));
+app.get('/', (req, res) => res.send('Hello Everyone, this is my starting code'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
